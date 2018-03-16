9c735fa504a7f32937bb9e03b24359eb20f3ff59
Claudio Stevanato <cstevanato@gmail.com>
2018-03-16 13:48:43 -0300

    Create new module test code share. Debug log

--- app/src/main/java/com/example/stv/logutilexample/MainActivity.java
+++ app/src/main/java/com/example/stv/logutilexample/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.stv.logutilexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.stv.logutil.LogDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogDebug.d("Teste de debug");
     }
 }
---
+++ errorutil/src/main/java/com/example/stv/errorutil/ErrorDebug.java
@@ -0,0 +1,15 @@
+package com.example.stv.errorutil;
+
+import android.util.Log;
+
+/**
+ * Created by Steva on 16/03/2018.
+ */
+
+public class ErrorDebug {
+    private static final String TAG = "SUPER_AWESOME_APP";
+
+    public static void e(String message) {
+        Log.e(TAG, message);
+    }
+}
